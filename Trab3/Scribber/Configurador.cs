ed53d22b335faff810c3321bd3d782cf14f257d1
Andre Basilio <andrebasilio123@gmail.com>
2013-06-24 23:51:06 +0100

    Scribber
    Coloquei a classe a chamar as duas forms (FALTA MUITA COISA)

--- Trab3/Scribber/Configurador.cs
+++ Trab3/Scribber/Configurador.cs
@@ -12,70 +12,30 @@ using Configuration;
 
 namespace netscribber
 {
-    
-    //class RecorderReplayer
-    //{
 
-    //    public static void Create(Configuration myConfig, Form formRecord)
-    //    {
-    //        //FAZER       
-    //    }
+    class recorderreplayer
+    {
 
 
-    //    public static Thread Execute(Form form)
-    //    {
-
-    //        //REFAZER IDEIA
-
-    //        Thread t = new Thread(() =>
-    //        {
-
-    //            Type typeForm = form.GetType();
-    //            IEnumerator eventForm = typeForm.GetRuntimeEvents().GetEnumerator();
-
-    //            //System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\test.txt");
-
-    //            while (eventForm.MoveNext())
-    //            {
-    //                EventInfo s2 = (EventInfo)eventForm.Current;
-    //         ///       file.Write(s2.Name);
-    //            }
-
-    //         //   file.Close();
-    //        });
-
-    //        //   t.Start();
-    //        return t;
-    //    }
 
 
-    //}
+
+    }
     class Program
     {
         static void Main()
         {
             var formPaint = new ScribberForm();
-            var formRecord = new FormRecorder();
+            var formRecord = new netscribber.FormRecorder();
 
             Configuration<Button> ic = new Configuration<Button>(formPaint);
 
-            ic.CustomConfiguration();
+            formRecord.Show();
 
-           // var csConfig = new Configuration(formPaint);
-            
-            /////////////////////////////////////////////////////
-            //csConfig.CustomConfiguration();
-
-
-            // var recorderService = RecorderReplayer.Create(csConfig,formRecord );
-
-           // RecorderReplayer.Execute(formPaint).Start();
-
-        //    Application.Run(formPaint);
+            Application.Run(formPaint);
         }
     }
 }
-
 
 
 
